28e0264225ad6ac61c7d8d55e30421e48e1050f6
pbiffle <patrick.biffle@dish.com>
2020-03-18 15:45:29 -0600

    changed port

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const blog = require('./routes/blog.js');
+const port = process.env.PORT || 3002
 require('dotenv').config();
 
 app.use(cors());
 app.use('/blog', blog);
 
-app.listen(3002, () => console.warn('yeet listening on 3002'))
+app.listen(port, () => console.warn(`yeet listening on ${port}`))
